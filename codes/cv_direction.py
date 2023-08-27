44b76d44542e22d98c226026a67308a105bf5962
joserigel <joserigel18@gmail.com>
2023-08-27 17:09:42 +0200

    Fixed minor

--- codes/cv_direction.py
+++ codes/cv_direction.py
@@ -1,13 +1,18 @@
 import cv2 as cv
+import numpy as np
+from scipy import ndimage
 
 # Read
 img = cv.imread("pattern.png")
+img = cv.GaussianBlur(img, ksize=(7,7), sigmaX=5)
 
-sobel = cv.Sobel(img, -1, 1, 0)
-scharr = cv.Scharr(img, -1, 1, 0)
+sobel = ndimage.sobel(img, axis=1)
+prewitt = ndimage.prewitt(img, axis=1)
+
+
 
 cv.imwrite("../img/directionPattern.png", img)
 cv.imwrite("../img/directionSobel.png", sobel)
-cv.imwrite("../img/directionScharr.png", scharr)
+cv.imwrite("../img/directionPrewitt.png", prewitt)
 
 
